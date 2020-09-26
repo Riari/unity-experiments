9317047dd3dac0b0556578cff2fb3a70c0ef39fb
Riari <3583774+Riari@users.noreply.github.com>
2020-09-27 00:34:53 +0100

    Initial commit

---
+++ unity/RPGSandbox/Assets/Scripts/GUIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIController : MonoBehaviour
+{
+    public GameObject SpellsPanel;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpellsPanel.SetActive(!SpellsPanel.activeSelf);
+        }
+    }
+}
